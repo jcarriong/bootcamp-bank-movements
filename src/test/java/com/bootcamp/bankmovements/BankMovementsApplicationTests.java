a105536a7fea7e48aeff05667643f1d2afe923da
jcarriong <finales.15@gmail.com>
2023-03-28 11:28:40 -0500

    first commit

---
+++ src/test/java/com/bootcamp/bankmovements/BankMovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankmovements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMovementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
